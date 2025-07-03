c28316ec1894e50f503d85524e9d75298f7e7f26
Mirxa893 <mirxakamran893@gmail.com>
2025-07-04 00:01:23 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
   return (
     <Router>
       <HeroUIProvider>
-        <ToastProvider>
+        <ToastProvider> {/* FIXED: Now properly wraps children */}
           <ThemeProvider>
             <AuthProvider>
               <Layout>
